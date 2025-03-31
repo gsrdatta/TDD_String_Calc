6f962e5eff21990989f0a7cd18e3036e2d50a947
Datta Gsr <sivaramadatta.g@chakravuyha.com>
2025-03-31 22:16:03 +0530

    Complete String Calculator

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   //Check for empty Strings
   if (numbers === "") return 0;
-  let sum = 0;
+
   //Added regex to check newline \n or comma
   let regx = /[\n,]/;
   if (numbers.startsWith("//")) {
@@ -9,14 +9,15 @@ function add(numbers) {
     regx = new RegExp("\\" + parts[0].slice(2));
     numbers = parts[1];
   }
+
   const numbersArray = numbers.split(regx).map((num) => parseInt(num, 10));
-  console.log(numbersArray);
+
   const negatives = numbersArray.filter((num) => num < 0);
   if (negatives.length > 0) {
     throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
   }
-  sum = numbersArray.reduce((sum, num) => sum + num, 0);
-  return sum;
+
+  return numbersArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
