d43fa3ad6f09256093de859f89b1290b33c0922c
Datta Gsr <sivaramadatta.g@chakravuyha.com>
2025-03-31 21:33:07 +0530

    case2:Support Single Number Input

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,5 +1,12 @@
 const add = require("./stringCalculator");
 
+//Handle empty String
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
+});
+
+//Handle single number
+test("returns the number itself when only one number is provided", () => {
+  expect(add("1")).toBe(1);
+  expect(add("5")).toBe(5);
 });
