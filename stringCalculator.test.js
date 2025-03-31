2133be8595f4719285f9a6781a3d39ef2a6ce445
Datta Gsr <sivaramadatta.g@chakravuyha.com>
2025-03-31 21:39:51 +0530

    case4: Add multiple numbers seperated by comma

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -16,3 +16,9 @@ test("returns sum of two numbers separated by a comma", () => {
   expect(add("1,2")).toBe(3);
   expect(add("10,20")).toBe(30);
 });
+
+//Handle morethan 2 numbers, Unknown no.of numbers
+test("returns sum of multiple numbers separated by a comma", () => {
+  expect(add("1,2,3")).toBe(6);
+  expect(add("5,10,15,20")).toBe(50);
+});
